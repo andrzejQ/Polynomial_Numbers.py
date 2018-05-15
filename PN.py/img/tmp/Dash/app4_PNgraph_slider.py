4093f56dc235c157fdb1031d7fe95cb1fbdcf299
Andrzej Kubaszek <kubaszek@prz.edu.pl>
2018-05-15 12:53:24 +0200

    Sliders 04 ok

--- PN.py/img/tmp/Dash/app4_PNgraph_slider.py
+++ PN.py/img/tmp/Dash/app4_PNgraph_slider.py
@@ -12,19 +12,6 @@ from PNlib.PolyNum import PolyNum
 f_1111 = 0.5 * PolyNum('const:(~1~,2~2~2~2~...~)')
 p_tr = PolyNum('const:(~2~,-4~4~-4~4~...~)') # 2*(~1~-1~)/(~1~1~)
 
-    
-h_b = [0.15,0.2] # diffrent sampling periods for graphs
-x_b = []
-T_0 = 0.04
-for h in h_b:
-    p = 1/h * p_tr
-    x_b += [ p * f_1111 * 1 / (p**2 + p + 4) * ( -(-T_0) * ( (p**2 +1).sqrt() ) ).exp() ]
-
-x_b0 = []
-for h in h_b:
-    p = 1/h * p_tr
-    x_b0 += [ p * f_1111 * 1 / (p**2 + p + 4) * ( -(-T_0) * ( (p**2 +1).sqrt() ) ).exp() * (-T_0*p).exp() ]
-
 import plotly.plotly as py
 import plotly.graph_objs as go
 style = [{"line": {"color": "rgba (255, 0, 0, 1)", "dash": "6px,3px", "width": 0.8}, 
@@ -36,12 +23,16 @@ style = [{"line": {"color": "rgba (255, 0, 0, 1)", "dash": "6px,3px", "width": 0
 
 app = dash.Dash()
 
-layout_a = go.Layout(autosize=False, width=800, height=220,
+layout_a = go.Layout(autosize=False, width=700, height=220,
     margin=go.Margin(l=50, r=50, b=40, t=20, pad=4),
     yaxis=dict(range=[-35, 35]), xaxis=dict(title='t',range=[0, 25])
 )
+layout_b = go.Layout(autosize=False, width=700, height=300,
+    margin=go.Margin(l=50, r=50, b=40, t=20, pad=4),
+    yaxis=dict(range=[-0.5, 0.5]), xaxis=dict(title='t',range=[0, 6.6])
+)
 
-def your_compute_figure_function(h_0, h_1): # arg: new_slider_value
+def compute_fig_x_a(h_0, h_1): # arg: new_slider_value
     h_a = [h_0,h_1] # diffrent sampling periods for graphs
     x_a = []
     for h in h_a:  # diffrent sampling periods
@@ -53,19 +44,32 @@ def your_compute_figure_function(h_0, h_1): # arg: new_slider_value
     new_figure_a={'data': go.Data(traces_a), 'layout': layout_a}
     return new_figure_a
 
-traces_b = []
-for n, h in enumerate(h_b):  # diffrent sampling periods
-    traces_b += [go.Scatter( y=list(x_b[n]), x0=0, dx=h, name='h='+str(h), **style[n] )]
-
-layout_b = go.Layout(autosize=False, width=800, height=300,
-    margin=go.Margin(l=50, r=50, b=40, t=20, pad=4),
-    yaxis=dict(range=[-0.5, 0.5]), xaxis=dict(title='t',range=[0, 6.6])
-)
-
-traces_b0 = []
-for n, h in enumerate(h_b):  # diffrent sampling periods
-    traces_b0 += [go.Scatter( y=list(x_b0[n]), x0=0, dx=h, name='h='+str(h), **style[n] )]
+T_0 = 0.04
     
+def compute_fig_x_b(h_0, h_1): # arg: new_slider_value
+    h_b = [h_0,h_1] # diffrent sampling periods for graphs
+    x_b = []
+    for h in h_b:
+        p = 1/h * p_tr
+        x_b += [ p * f_1111 * 1 / (p**2 + p + 4) * ( -(-T_0) * ( (p**2 +1).sqrt() ) ).exp() ]
+    traces_b = []
+    for n, h in enumerate(h_b):  # diffrent sampling periods
+        traces_b += [go.Scatter( y=list(x_b[n]), x0=0, dx=h, name='h='+str(h), **style[n] )]
+    new_figure_b={'data': go.Data(traces_b), 'layout': layout_b}
+    return new_figure_b
+    
+def compute_fig_x_b0(h_0, h_1): # arg: new_slider_value
+    h_b0 = [h_0,h_1] # diffrent sampling periods for graphs
+    x_b0 = []
+    for h in h_b0:
+        p = 1/h * p_tr
+        x_b0 += [ p * f_1111 * 1 / (p**2 + p + 4) * ( -(-T_0) * ( (p**2 +1).sqrt() ) ).exp() * (-T_0*p).exp() ]
+    traces_b0 = []
+    for n, h in enumerate(h_b0):  # diffrent sampling periods
+        traces_b0 += [go.Scatter( y=list(x_b0[n]), x0=0, dx=h, name='h='+str(h), **style[n] )]
+    new_figure_b0={'data': go.Data(traces_b0), 'layout': layout_b}
+    return new_figure_b0
+        
 app.layout = html.Div([
     dcc.Markdown(children='''\
 # Discrete representations of generalized time domain functions
@@ -94,13 +98,17 @@ for h in h_a:  # diffrent sampling periods
     p = 1/h * p_tr
     x_a += [ p * f_1111 * p ]
 ```
+
 '''
     ),
-    html.Label('h_0, h_1'),
-    dcc.RangeSlider(id='sli_h', value=[0.4, 0.8], min=0.3, max=0.9, step=0.05, updatemode='drag'),
+    html.Label('Change values of sampling step h: o ------------------ o'),
+    dcc.RangeSlider(
+        id='sli_h_a', value=[0.4, 0.8], min=0.3, max=0.9, step=0.05, updatemode='drag',
+        className='sli_h'
+    ),
     dcc.Graph(
         id='grf2_44_44',
-        figure=your_compute_figure_function(0.5, 0.7)
+        figure=compute_fig_x_a(0.5, 0.7)
     ),
     dcc.Markdown(children='''\
 ---
@@ -116,10 +124,15 @@ for h in h_b:
 ```
 '''
     ),
+    html.Label('Change values of sampling step h: o ------------------ o'),
+    dcc.RangeSlider(
+        id='sli_h_b', value=[0.15, 0.2], min=0.1, max=0.3, step=0.02, updatemode='drag',
+        className='sli_h'
+    ),
     dcc.Graph(
         id='grf_exp1',
         figure={
-            'data': go.Data(traces_b),
+            'data': compute_fig_x_b(0.1, 0.2),
             'layout': layout_b
         }
     ),
@@ -135,19 +148,39 @@ for h in h_b:
 ```
 '''
     ),
+    html.Label('Change values of sampling step h: o ------------------ o'),
+    dcc.RangeSlider(
+        id='sli_h_b0', value=[0.15, 0.2], min=0.1, max=0.3, step=0.02, updatemode='drag',
+        className='sli_h'
+    ),
     dcc.Graph(
         id='grf_exp1zero',
         figure={
-            'data': go.Data(traces_b0),
+            'data': compute_fig_x_b0(0.1, 0.2),
             'layout': layout_b
         }
     )
-])
+], 
+className="container",
+style={'width':700}
+)
 
 @app.callback(Output('grf2_44_44', 'figure'),
-             [Input('sli_h', 'value')])
-def your_data_analysis_function(new_slider_values):
-    new_figure = your_compute_figure_function(*new_slider_values)
+             [Input('sli_h_a', 'value')])
+def data_analysis_x_a(new_slider_values):
+    new_figure = compute_fig_x_a(*new_slider_values)
+    return new_figure
+
+@app.callback(Output('grf_exp1', 'figure'),
+             [Input('sli_h_b', 'value')])
+def data_analysis_x_b(new_slider_values):
+    new_figure = compute_fig_x_b(*new_slider_values)
+    return new_figure
+
+@app.callback(Output('grf_exp1zero', 'figure'),
+             [Input('sli_h_b0', 'value')])
+def data_analysis_x_b0(new_slider_values):
+    new_figure = compute_fig_x_b0(*new_slider_values)
     return new_figure
 
 if __name__ == '__main__':
