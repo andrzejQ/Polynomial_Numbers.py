e3f73da92cf9a2c40d7e52637d8c27652370b826
Andrzej Kubaszek <kubaszek@prz.edu.pl>
2018-05-15 19:46:49 +0200

    Black box 0

--- PN.py/04h.PN generalized tm fun.-dash.py
+++ PN.py/04h.PN generalized tm fun.-dash.py
@@ -31,6 +31,14 @@ layout_b = go.Layout(autosize=False, width=700, height=300,
     margin=go.Margin(l=50, r=50, b=40, t=20, pad=4),
     yaxis=dict(range=[-0.5, 0.5]), xaxis=dict(title='t',range=[0, 6.6])
 )
+layout_d_x = go.Layout(autosize=False, width=700, height=200,
+    margin=go.Margin(l=50, r=50, b=40, t=20, pad=4),
+    yaxis=dict(range=[-90000, 90000]), xaxis=dict(title='t',range=[0, 4.6])
+)
+layout_d_y = go.Layout(autosize=False, width=700, height=200,
+    margin=go.Margin(l=50, r=50, b=40, t=20, pad=4),
+    yaxis=dict(range=[-3.0, 2.0]), xaxis=dict(title='t',range=[0, 4.6])
+)
 
 def compute_fig_x_a(h_0, h_1): # arg: new_slider_value
     h_a = [h_0,h_1] # diffrent sampling periods for graphs
@@ -69,7 +77,21 @@ def compute_fig_x_b0(h_0, h_1): # arg: new_slider_value
         traces_b0 += [go.Scatter( y=list(x_b0[n]), x0=0, dx=h, name='h='+str(h), **style[n] )]
     new_figure_b0={'data': go.Data(traces_b0), 'layout': layout_b}
     return new_figure_b0
-        
+    
+def compute_fig_xy_d(a, h, xy='x'): # arg: new_slider_value
+    p = 1/h * p_tr
+    blackBox1 = 1 / (p**2 + p + 4)
+    x_ =  f_1111 * p**(a+1)
+    if xy == 'x':
+        layout_d = layout_d_x
+        traces_d = [ go.Scatter( y=list(x_), x0=0, dx=h, name='x_d(t), a='+str(a)+', h='+str(h), **style[0] ) ]
+    else:
+        y_ = x_ * blackBox1
+        layout_d = layout_d_y
+        traces_d = [ go.Scatter( y=list(y_), x0=0, dx=h, name='y_d(t), a='+str(a)+', h='+str(h), **style[1] ) ]
+    new_figure_d={'data': go.Data(traces_d), 'layout': layout_d}
+    return new_figure_d
+
 app.layout = html.Div([
     dcc.Markdown(children=r'''
 # Discrete representations of generalized time domain functions
@@ -145,7 +167,7 @@ for h in h_a:  # diffrent sampling periods
         id='grf2_44_44',
         figure=compute_fig_x_a(0.5, 0.7)
     ),
-    dcc.Markdown(children='''\
+    dcc.Markdown(children=r'''
 ---
 ### Digital samples of signal containing negative delay operator:
 
@@ -178,7 +200,7 @@ for h in h_b:
             'layout': layout_b
         }
     ),
-    dcc.Markdown(children='''\
+    dcc.Markdown(children=r'''
 ---
 Neutralizing negative delay operator we get regular function for $t ≥ 0$
  (see the last part of [Mikusinski's remarks about negative delay operator](http://www.pei.prz.edu.pl/~kubaszek/smacd06/JM_OperCalc.html)):
@@ -209,7 +231,47 @@ for h in h_b:
             'data': compute_fig_x_b0(0.1, 0.2),
             'layout': layout_b
         }
+    ),
+    dcc.Markdown(children=r'''
+---    
+# Black box parameters
+---    
+
+## Derivative of the step function on input of black box  
+
+{*x_d(t)*} = { (d/dt)^*a* *δ(t*) } = *p* · {1} · *p*^*a*
+
+Sequences of the samples, which correspond to this abstract functions - a-th derivative of Dirac's delta function can be used to filter characteristic of DSP algorithm closed in black box.
+
+    
+''' ),
+    html.Div([
+        html.Label('Change derivative level _a_:'),
+        dcc.Slider(
+            id='sli_a', value=2.0, min=-1.0, max=2.6, step=0.1, 
+            updatemode='drag', className='sli_h'),
+        html.Label('Change sampling step _h_:'),
+        dcc.Slider(
+            id='sli_h_d', value=0.15, min=0.05, max=0.2, step=0.01, 
+            updatemode='drag', className='sli_h')
+        ],
+        style={'width':600, 'padding':10}
+    ),
+    dcc.Graph(
+        id='grf_d_x',
+        figure={
+            'data': compute_fig_xy_d(2.0, 0.15, 'x'),
+            'layout': layout_d_x
+        }
+    ),
+    dcc.Graph(
+        id='grf_d_y',
+        figure={
+            'data': compute_fig_xy_d(2.0, 0.15, 'y'),
+            'layout': layout_d_y
+        }
     )
+
 ], 
 className="container"
 )
@@ -232,6 +294,19 @@ def data_analysis_x_b0(new_slider_values):
     new_figure = compute_fig_x_b0(*new_slider_values)
     return new_figure
 
+@app.callback(Output('grf_d_x', 'figure'),
+             [Input('sli_a', 'value'), Input('sli_h_d', 'value')])
+def data_analysis_xy_d(a, h, xy='x'):
+    new_figure = compute_fig_xy_d(a, h, xy='x')
+    return new_figure
+
+@app.callback(Output('grf_d_y', 'figure'),
+             [Input('sli_a', 'value'), Input('sli_h_d', 'value')])
+def data_analysis_xy_d(a, h, xy='y'):
+    new_figure = compute_fig_xy_d(a, h, xy='y')
+    return new_figure
+
+    
 if __name__ == '__main__':
     app.run_server()
 #√·∫≠≤≥ ⁰¹²³⁴⁵⁶⁸⁹⁺⁻ⁱⁿ⁽⁾ ₀₁₂₃₄₆₇₈₉₊₋₌₎ₐₑₒ ¼½ δ
