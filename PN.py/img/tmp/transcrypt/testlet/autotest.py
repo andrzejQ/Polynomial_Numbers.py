29a2a35b451886c74184e39ee5450feaeb1ee492
Andrzej Kubaszek <kubaszek@prz.edu.pl>
2018-05-29 12:45:31 +0200

    x=PolyNum('~1~2~') breaks autotest....

--- PN.py/img/tmp/transcrypt/testlet/autotest.py
+++ PN.py/img/tmp/transcrypt/testlet/autotest.py
@@ -1,8 +1,14 @@
 import org.transcrypt.autotester
 
+import testlet0
+import testlet1sin
+
 import testletPN0
 
 autoTester = org.transcrypt.autotester.AutoTester ()
+
+autoTester.run (testlet0, 'testlet0')
+autoTester.run (testlet1sin, 'testlet1sin')
 
 autoTester.run (testletPN0, 'testletPN0')
 
